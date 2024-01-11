71a9a0aa34bdef2498ee00d521f447bb72863c34
samandar-me <samandarcoderdev@gmail.com>
2024-01-11 16:02:31 +0500

    j

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quiz_game/bloc/game_bloc.dart';
 import 'package:quiz_game/screen/home_page.dart';
 
 void main() {
@@ -10,12 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => GameBloc())
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_game/bloc/game_bloc.dart';
 import 'package:quiz_game/bloc/game_event.dart';
+import 'package:quiz_game/bloc/game_state.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,46 +28,63 @@ class _HomePageState extends State<HomePage> {
             ]
           )
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 100),
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-                child: Image.network("https://www.thesafaricollection.com/wp-content/uploads/2022/07/The-Safari-Collection-Hey-You-Giraffe-Manor-1.jpg",
-                  width: MediaQuery.of(context).size.width - 100,fit: BoxFit.cover,),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+        child: BlocConsumer<GameBloc, GameState>(
+          listener: (context, state) {
+            if(state is GameFinished) {
+
+            } else if(state is Success) {
+              print(state.currentQuestion.correctAnswer);
+            }
+          },
+          builder: (context, state) {
+            if(state is Loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return _buildBody();
+          },
+        )
+      ),
+    );
+  }
+  Widget _buildBody() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 100),
+        Expanded(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.network("https://www.thesafaricollection.com/wp-content/uploads/2022/07/The-Safari-Collection-Hey-You-Giraffe-Manor-1.jpg",
+              width: MediaQuery.of(context).size.width - 100,fit: BoxFit.cover,),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: Text("Bear",style: TextStyle(color: Colors.black),)),
-                      ElevatedButton(onPressed: () {}, child: Text("Lion",style: TextStyle(color: Colors.black),)),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(onPressed: () {}, child: Text("Dragon",style: TextStyle(color: Colors.black),)),
-                      ElevatedButton(onPressed: () {}, child: Text("Giraffe",style: TextStyle(color: Colors.black),)),
-                    ],
-                  ),
-                  ElevatedButton(onPressed: () {
-                    /// tezro qo'chqar
-                    BlocProvider.of<GameBloc>(context).add(OnNextQuestion());
-                  }, child: Text("NEXT"))
+                  ElevatedButton(onPressed: () {}, child: Text("Bear",style: TextStyle(color: Colors.black),)),
+                  ElevatedButton(onPressed: () {}, child: Text("Lion",style: TextStyle(color: Colors.black),)),
                 ],
               ),
-            )
-          ],
-        ),
-      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: () {}, child: Text("Dragon",style: TextStyle(color: Colors.black),)),
+                  ElevatedButton(onPressed: () {}, child: Text("Giraffe",style: TextStyle(color: Colors.black),)),
+                ],
+              ),
+              ElevatedButton(onPressed: () {
+                /// tezro qo'chqar
+                BlocProvider.of<GameBloc>(context).add(OnNextQuestion());
+              }, child: Text("NEXT"))
+            ],
+          ),
+        )
+      ],
     );
   }
 }
